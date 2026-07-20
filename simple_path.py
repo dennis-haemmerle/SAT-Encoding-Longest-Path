f4d5546322c216232316ae891257b56588d1f6fd
dennis-haemmerle <dh348@email.uni-freiburg.de>
2026-07-21 00:04:27 +0200

    edge encoding with DFJ subtour elimination

--- simple_path.py
+++ simple_path.py
@@ -85,9 +85,9 @@ def simple_path_of_length_k(G: nx.Graph, k: int, start=None, end=None, only_in=N
 
     # Set optional start/end nodes
     if start:
-        cnf.append([vpool.id((s, 0)) for s in start])
+        cnf.append([vpool.id((v, 0)) for v in start])
     if end:
-        cnf.append([vpool.id((e, k)) for e in end])
+        cnf.append([vpool.id((v, k)) for v in end])
 
     # Symmetry breaking
     if symmetry is not None:
@@ -112,6 +112,213 @@ def simple_path_of_length_k(G: nx.Graph, k: int, start=None, end=None, only_in=N
         if solver.solve():
             model = set(solver.get_model())  # type: ignore
             assignment = [v for i in range(k + 1) for v in G.nodes() if vpool.id((v, i)) in model]
+            return assignment
+        return None
+
+
+def simple_path_of_length_k_edge_encoding(G: nx.Graph, k: int):
+    if k < 0 or k > G.number_of_edges():
+        return None
+    if k == 0:
+        return [] if G.number_of_nodes() > 0 else None
+
+    vpool = IDPool()
+    cnf = CNF()
+
+    def edge_var(e):
+        u, v = e
+        if G.is_directed():
+            return vpool.id((u, v))
+        return vpool.id((min(u, v), max(u, v)))
+
+    # 1. Exactly k edges
+    lits = [edge_var(e) for e in G.edges()]
+    cnf.extend(CardEnc.equals(lits=lits, bound=k, vpool=vpool, encoding=EncType.seqcounter).clauses)
+
+    # 2. Exactly k+1 nodes
+    lits = [vpool.id(v) for v in G.nodes()]
+    cnf.extend(CardEnc.equals(lits=lits, bound=k + 1, vpool=vpool, encoding=EncType.seqcounter).clauses)
+
+    # 3. Atleast one incident edge for each used node
+    for v in G.nodes():
+        if G.is_directed():
+            incident = ([edge_var(e) for e in G.in_edges(v)] + [edge_var(e) for e in G.out_edges(v)])  # type: ignore
+        else:
+            incident = [edge_var(e) for e in G.edges(v)]
+
+        for e in incident:
+            cnf.append([-e, vpool.id(v)])  # e -> v
+        cnf.append([-vpool.id(v)] + incident)  # v -> atleast one incident
+
+    # 4. Node degree atmost 2
+    for v in G.nodes():
+        if G.is_directed():
+            incoming = [edge_var(e) for e in G.in_edges(v)]  # type: ignore
+            outgoing = [edge_var(e) for e in G.out_edges(v)]  # type: ignore
+
+            block = CardEnc.atmost(lits=incoming, bound=1, vpool=vpool, encoding=EncType.seqcounter)
+            cnf.extend(block.clauses)
+            block = CardEnc.atmost(lits=outgoing, bound=1, vpool=vpool, encoding=EncType.seqcounter)
+            cnf.extend(block.clauses)
+        else:
+            incident = [edge_var(e) for e in G.edges(v)]
+            block = CardEnc.atmost(lits=incident, bound=2, vpool=vpool, encoding=EncType.seqcounter)
+            cnf.extend(block.clauses)
+
+    # 5. Exactly two endpoints with degree 1
+    if G.is_directed():
+        start_vars = []
+        end_vars = []
+
+        for v in G.nodes():
+            incoming = [edge_var(e) for e in G.in_edges(v)]  # type: ignore
+            outgoing = [edge_var(e) for e in G.out_edges(v)]  # type: ignore
+
+            # start_var = True <=> (indeg(v) = 0 ∧ outdeg(v) = 1)
+            start_var = vpool.id(("start", v))
+            start_vars.append(start_var)
+            # end_var = True <=> (indeg(v) = 1 ∧ outdeg(v) = 0)
+            end_var = vpool.id(("end", v))
+            end_vars.append(end_var)
+
+            # start_var -> outdeg(v) = 1
+            if len(outgoing) == 0:
+                cnf.append([-start_var])
+            else:
+                exactly_one_out = CardEnc.equals(lits=outgoing, bound=1, vpool=vpool, encoding=EncType.seqcounter)
+                for clause in exactly_one_out.clauses:
+                    cnf.append([-start_var] + clause)
+
+            # start_var -> indeg(v) = 0
+            for e in incoming:
+                cnf.append([-start_var, -e])
+
+            # end_var -> indeg(v) = 1
+            if len(incoming) == 0:
+                cnf.append([-end_var])
+            else:
+                exactly_one_in = CardEnc.equals(lits=incoming, bound=1, vpool=vpool, encoding=EncType.seqcounter)
+                for clause in exactly_one_in.clauses:
+                    cnf.append([-end_var] + clause)
+
+            # end_var -> outdeg(v) = 0
+            for e in outgoing:
+                cnf.append([-end_var, -e])
+
+            out_deg_1 = vpool.id(("out_deg_1", v))
+            if not outgoing:
+                cnf.append([-out_deg_1])
+            else:
+                # out_deg_1 -> outdeg(v) = 1
+                exactly_one_out = CardEnc.equals(lits=outgoing, bound=1, vpool=vpool, encoding=EncType.seqcounter)
+                for clause in exactly_one_out.clauses:
+                    cnf.append([-out_deg_1] + clause)
+
+                # outdeg(v) = 1 -> out_deg_1
+                for i, e in enumerate(outgoing):
+                    # exactly one outgoing -> out_deg_1
+                    cnf.append([-e] + outgoing[:i] + outgoing[i + 1:] + [out_deg_1])
+
+                for i in range(len(outgoing)):
+                    for j in range(i + 1, len(outgoing)):
+                        # atmost one outgoing -> out_deg_1
+                        cnf.append([-outgoing[i], -outgoing[j], -out_deg_1])
+
+            # (indeg(v) = 0 ∧ outdeg(v) = 1) -> start_var
+            if incoming:
+                cnf.append([-out_deg_1] + incoming + [start_var])
+            else:
+                cnf.append([-out_deg_1, start_var])
+
+            in_deg_1 = vpool.id(("in_deg_1", v))
+            if not incoming:
+                cnf.append([-in_deg_1])
+            else:
+                # in_deg_1 -> indeg(v) = 1
+                exactly_one_in = CardEnc.equals(lits=incoming, bound=1, vpool=vpool, encoding=EncType.seqcounter)
+                for clause in exactly_one_in.clauses:
+                    cnf.append([-in_deg_1] + clause)
+
+                # indeg(v) = 1 -> in_deg_1
+                for i, e in enumerate(incoming):
+                    # exactly one incoming -> in_deg_1
+                    cnf.append([-e] + incoming[:i] + incoming[i + 1:] + [in_deg_1])
+
+                for i in range(len(incoming)):
+                    for j in range(i + 1, len(incoming)):
+                        # atmost one incoming -> in_deg_1
+                        cnf.append([-incoming[i], -incoming[j], -in_deg_1])
+
+            # (indeg(v) = 1 ∧ outdeg(v) = 0) -> end_var
+            if outgoing:
+                cnf.append([-in_deg_1] + outgoing + [end_var])
+            else:
+                cnf.append([-in_deg_1, end_var])
+
+            # cnf.append([-start_var, -end_var])
+
+            # (start_var v end_var) -> v
+            cnf.append([-start_var, vpool.id(v)])
+            cnf.append([-end_var, vpool.id(v)])
+    else:
+        endpoint_vars = []
+
+        for v in G.nodes():
+            incident = [edge_var(e) for e in G.edges(v)]
+
+            # end_var = True <=> deg(v) = 1
+            end_var = vpool.id(("end", v))
+            # endpoint_vars.append(end_var)
+            endpoint_vars.append(end_var)
+
+            if not incident:
+                cnf.append([-end_var])
+                continue
+
+            # end_var -> deg(v) = 1
+            exactly_one = CardEnc.equals(lits=incident, bound=1, vpool=vpool, encoding=EncType.seqcounter)
+            for clause in exactly_one.clauses:
+                cnf.append([-end_var] + clause)
+
+            # deg(v) = 1 -> end_var
+            for i, e in enumerate(incident):
+                # atleast one incident -> end_var
+                cnf.append([-e] + incident[:i] + incident[i + 1:] + [end_var])
+
+            for i in range(len(incident)):
+                for j in range(i + 1, len(incident)):
+                    # atmost one incident -> end_var
+                    cnf.append([-incident[i], -incident[j], -end_var])
+
+            # end_var -> v
+            cnf.append([-end_var, vpool.id(v)])
+
+    if G.is_directed():
+        cnf.extend(CardEnc.equals(lits=start_vars, bound=1, vpool=vpool, encoding=EncType.seqcounter).clauses)
+        cnf.extend(CardEnc.equals(lits=end_vars, bound=1, vpool=vpool, encoding=EncType.seqcounter).clauses)
+    else:
+        cnf.extend(CardEnc.equals(lits=endpoint_vars, bound=2, vpool=vpool, encoding=EncType.seqcounter).clauses)
+
+    # 6. Subtour Elimination with DFJ
+    for r in range(2, G.number_of_nodes()):
+        for subset_nodes in combinations(G.nodes(), r):
+            subset_nodes = set(subset_nodes)
+
+            subset_edges = []
+            for u, v in G.edges():
+                if u in subset_nodes and v in subset_nodes:
+                    subset_edges.append(edge_var((u, v)))
+
+            diff = len(subset_edges) - len(subset_nodes)
+            if diff >= 0:
+                max_true = len(subset_edges) - (diff + 1)
+                block = CardEnc.atmost(lits=subset_edges, bound=max_true, vpool=vpool, encoding=EncType.seqcounter)
+                cnf.extend(block.clauses)
+
+    with Solver(name="Cadical195", bootstrap_with=cnf.clauses) as solver:
+        if solver.solve():
+            model = set(solver.get_model())  # type: ignore
+            assignment = [e for e in G.edges() if edge_var(e) in model]
             return assignment
         return None
 
