0bfe52fbe1abaa3403ad6d5726cac80417ec3bf1
dennis-haemmerle <dh348@email.uni-freiburg.de>
2026-07-29 23:04:06 +0200

    update incremental position encoding

--- incremental_simple_path.py
+++ incremental_simple_path.py
@@ -1,6 +1,6 @@
 import networkx as nx
 from pysat.formula import IDPool
-from pysat.card import CardEnc, EncType
+from pysat.card import CardEnc, EncType, ITotalizer
 from pysat.solvers import Solver
 
 
@@ -11,6 +11,14 @@ class IncrementalSimplePathEncoder:
         self.vpool = IDPool()
         self.solver = Solver(name="Cadical195")
         self.max_k = -1
+
+    def to_set(self, val):
+        if val is None:
+            return set()
+        elif isinstance(val, (list, set)):
+            return set(val)
+        else:
+            return {val}
 
     def extend_to(self, k: int):
         if k <= self.max_k:
@@ -36,18 +44,53 @@ class IncrementalSimplePathEncoder:
 
             self.max_k = i
 
-    def solve(self, k: int):
+    def solve(self, k: int, assumptions: list):
         if k < 0 or k > self.G.number_of_edges():
             return False
 
         self.extend_to(k)
 
-        return bool(self.solver.solve())
+        return bool(self.solver.solve(assumptions=assumptions))
 
-    def longest_simple_path(self):
+    def longest_simple_path(self, start=None, end=None, symmetry=None):
+        self.start = self.to_set(start)
+        self.end = self.to_set(end)
+
+        if start:
+            self.solver.add_clause([self.vpool.id((v, 0)) for v in self.start])
+
+        # Symmetry breaking
+        if symmetry is not None:
+            orbit_groups = symmetry.get("orbit_groups", {})
+            for orbit in orbit_groups.values():
+                if len(orbit) <= 1:
+                    continue
+
+                # Only the representative of each orbit is allowed to be the start node.
+                starters = [v for v in orbit if v in self.start] if self.start else list(orbit)
+                if not starters:
+                    continue
+
+                representative = min(starters)
+
+                for v in orbit:
+                    if v != representative:
+                        self.solver.add_clause([-self.vpool.id((v, 0))])
+
         longest_path = []
+
         for k in range(1, min(self.G.number_of_nodes(), self.G.number_of_edges() + 1)):
-            if self.solve(k):
+            assumptions = []
+
+            if self.end:
+                if len(self.end) == 1:
+                    assumptions.append(self.vpool.id((next(iter(self.end)), k)))
+                else:
+                    for v in self.G.nodes():
+                        if v not in self.end:
+                            assumptions.append(-self.vpool.id((v, k)))
+
+            if self.solve(k, assumptions):
                 model = set(self.solver.get_model())  # type: ignore
                 longest_path = [v for i in range(k + 1) for v in self.G.nodes() if self.vpool.id((v, i)) in model]
             else:
